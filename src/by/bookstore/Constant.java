2610a98c38828a3ce486ca3d970ee317cf3ebeab
Artsiom Smirnou <artem-artem-smirnov-1999-smirno@mail.ru>
2021-03-03 15:11:51 +0300

    add some line

--- src/by/bookstore/Constant.java
+++ src/by/bookstore/Constant.java
@@ -7,4 +7,5 @@ public class Constant {
     public static final String DB_URL = "jdbc:postgresql://localhost:5432/postgres";
     public static final String DB_LOGIN = "postgres";
     public static final String DB_PASSWORD = "root";
+    public static final String DB_PASSWORD2 = "root";
 }
